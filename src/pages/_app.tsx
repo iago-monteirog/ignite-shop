0f49884a936d633510efc775822062d0c8fd3c01
iago-monteirog <iagomgarcia9000@gmail.com>
2023-09-05 10:41:36 -0300

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,27 @@
+import { AppProps } from 'next/app'
+import { Roboto } from 'next/font/google'
+import { globalStyles } from '../styles/global'
+import logoImg from '../assets/logo.svg';
+import { Container, Header } from '../styles/pages/app';
+import Image from 'next/image';
+
+const roboto = Roboto({
+  weight: ['400', '700'],
+  style: ['normal'],
+  subsets: ['latin']
+})
+
+globalStyles();
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <main className={roboto.className}>
+      <Container>
+        <Header>
+          <Image src={logoImg} alt="" />
+        </Header>
+        <Component {...pageProps} />
+      </Container>
+    </main>
+  )
+}
